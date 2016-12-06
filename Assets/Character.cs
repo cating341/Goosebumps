d3c7c83a15f784ac402e68cfe5ef072a0baa7352
tzutzu <gatto0616@gmail.com>
2016-12-06 19:57:30 +0800

    Add Character move script

---
+++ Assets/Character.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Character : MonoBehaviour
+{
+    public float maxSpeed = 10.0f;
+    public float jumpForce = 800.0f;
+
+    public bool airControl = true;
+
+    bool facingRight;
+
+    public LayerMask groundLayer;
+
+    Transform groundCheck;
+    float groundRadius;
+    bool onGround;
+
+    Animator anim;
+
+    void Awake()
+    {
+        //get references
+        groundCheck = transform.Find("GroundCheck");
+       // anim = GetComponent<Animator>();
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        facingRight = true;
+        groundRadius = 0.1f;
+        onGround = false;
+    }
+
+    void FixedUpdate()
+    {
+        
+         //change the character animation by onGround state
+       // anim.SetBool("onGround", onGround);
+    }
+
+    public void Move(float movingSpeed, bool jump)
+    {
+        //left / right moving actived only when the character is on the ground or air control is premitted
+        if (onGround || airControl)
+        {
+            //change the character animation by moving speed
+            // anim.SetFloat("Speed", Mathf.Abs(movingSpeed));
+
+            //move the character
+            //only change its velocity on x axis
+            transform.position += new Vector3(movingSpeed * maxSpeed, GetComponent<Rigidbody>().velocity.y * Time.deltaTime, 0);
+            // GetComponent<Rigidbody>().velocity = new Vector2(movingSpeed * maxSpeed, GetComponent<Rigidbody>().velocity.y);
+
+            //flip the character image if player input direction is different with character's facing direction
+            if (movingSpeed > 0 && !facingRight || movingSpeed < 0 && facingRight) Flip();
+        }
+
+        //let character jump when it's on the ground and player hits jump button
+        if (onGround && jump)
+        {
+            //anim.SetBool("onGround", false);
+
+            //make character jump by adding force
+            GetComponent<Rigidbody>().AddForce(new Vector3(0.0f, jumpForce, 0.0f));
+        }
+    }
+
+    void Flip()
+    {
+        //reverse the direction
+        facingRight = !facingRight;
+
+        //flip the character by multiplying x local scale with -1
+        Vector3 characterScale = transform.localScale;
+        characterScale.x *= -1;
+        transform.localScale = characterScale;
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Ground")
+        {
+            onGround = true;
+        }
+    }
+}
