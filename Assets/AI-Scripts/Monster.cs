c003912a7dadce4a158bbcd2a8b2330ae8b1f72d
ShaoAn Chien <sac840711@gmail.com>
2016-12-08 00:36:29 +0800

    nothing

---
+++ Assets/AI-Scripts/Monster.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Monster : MonoBehaviour {
+
+    private int movingForward = 1;
+
+	// Use this for initialization
+	void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        GetComponent<Rigidbody>().velocity = new Vector3(2 * movingForward, 0, 0);
+        if (GetComponent<Rigidbody>().position.x > 10)
+        {
+            movingForward = -1;
+        }
+        else if (GetComponent<Rigidbody>().position.x < -10)
+        {
+            movingForward = 1;
+        }
+        print(GetComponent<Rigidbody>().position.x);
+    }
+}
