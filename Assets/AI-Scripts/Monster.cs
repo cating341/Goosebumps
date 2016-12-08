4f6218ed295ae0ab499693eefe5a1fa8021c3a1b
ShaoAn Chien <sac840711@gmail.com>
2016-12-08 23:36:12 +0800

    buggggg

--- Assets/AI-Scripts/Monster.cs
+++ Assets/AI-Scripts/Monster.cs
@@ -3,24 +3,81 @@ using System.Collections;
 
 public class Monster : MonoBehaviour {
 
-    private int movingForward = 1;
+    public float maxSpeed = 0.1f;
+    public float jumpForce = 100f;
 
-	// Use this for initialization
-	void Start () {
+    public bool airControl = true;
+
+    bool facingRight;
+
+    public LayerMask groundLayer;
+    float groundRadius;
+    bool onGround;
+    Animator anim;
+
+    void Awake()
+    {
+
     }
-	
-	// Update is called once per frame
-	void Update () {
 
-        //GetComponent<Rigidbody>().velocity = new Vector3(2 * movingForward, 0, 0);
-        if (GetComponent<Rigidbody>().position.x > 10)
+    // Use this for initialization
+    void Start ()
+    {
+        anim = GetComponent<Animator>();
+        facingRight = true;
+        groundRadius = 0.1f;
+        onGround = false;
+    }
+
+    public void Move(float movingSpeed, bool jump)
+    {
+        //left / right moving actived only when the character is on the ground or air control is premitted
+        if (onGround || airControl)
         {
-            movingForward = -1;
+            //change the character animation by moving speed
+            anim.SetFloat("speed", Mathf.Abs(movingSpeed));
+            print(anim.GetFloat("speed"));
+
+            //move the character
+            //only change its velocity on x axis
+            transform.position += new Vector3(movingSpeed * maxSpeed, GetComponent<Rigidbody>().velocity.y * Time.deltaTime, 0);
+            // GetComponent<Rigidbody>().velocity = new Vector2(movingSpeed * maxSpeed, GetComponent<Rigidbody>().velocity.y);
+
+            //flip the character image if player input direction is different with character's facing direction
+            if (movingSpeed > 0 && !facingRight || movingSpeed < 0 && facingRight) Flip();
         }
-        else if (GetComponent<Rigidbody>().position.x < -10)
+
+        //let character jump when it's on the ground and player hits jump button
+        if (onGround && jump)
         {
-            movingForward = 1;
+
+            //make character jump by adding force
+            GetComponent<Rigidbody>().AddForce(new Vector3(0.0f, jumpForce, 0.0f));
         }
-        print(GetComponent<Rigidbody>().position.x);
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+
+
+    void Flip()
+    {
+        //reverse the direction
+        facingRight = !facingRight;
+
+        //flip the character by multiplying x local scale with -1
+        Vector3 characterScale = transform.localScale;
+        characterScale.x *= -1;
+        transform.localScale = characterScale;
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Ground")
+        {
+            onGround = true;
+        }
     }
 }
---
+++ Assets/AI-Scripts/MonsterControl.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MonsterControl : MonoBehaviour {
+
+    private Monster monster;
+    private bool jump;
+    private float movingSpeed;
+
+    // Use this for initialization
+    void Start()
+    {
+        monster = GetComponent<Monster>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void FixedUpdate()
+    {
+        //get input by Axis set in input setting
+        movingSpeed = 1f;
+
+        //pass parameters to character script, and then it can move
+        monster.Move(movingSpeed, jump);
+
+        //jump is reset after each time that physical engine updated
+        jump = false;
+    }
+}
