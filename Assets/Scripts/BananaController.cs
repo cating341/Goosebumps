c07bf768b96a28866805851ce712898c0a8f4007
blurry fish <acht7111020@gmail.com>
2016-12-20 02:15:11 +0800

    add lighting stone and magic tree

---
+++ Assets/Scripts/BananaController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BananaController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.tag == "Monster" || other.gameObject.tag == "Player")
+        {
+
+            Debug.Log("Trigger banana event");
+        }
+    }
+}
