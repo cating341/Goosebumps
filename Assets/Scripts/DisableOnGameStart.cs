86b889351606555c71ebd487cd620c2083e8e156
Shawn <san.an.lee@gmail.com>
2016-12-21 17:07:27 +0800

    Disable Ladder on game start

---
+++ Assets/Scripts/DisableOnGameStart.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DisableOnGameStart : MonoBehaviour {
+	private MySceneManager sm;
+	// Use this for initialization
+	void Start () {
+		if (GetComponent<ItemOnObject> ().item.itemName == "Ladder") {
+			sm = GameObject.Find ("SceneManager").GetComponent<MySceneManager> ();
+		} else {
+			this.enabled = false;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (sm.GameSceneIsGame()) {
+			Destroy (gameObject);
+		}
+	}
+}
