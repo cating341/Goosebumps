dddc59df805f705c32ad27be23d68cdbdac2dc6c
Shawn <san.an.lee@gmail.com>
2016-12-25 17:25:49 +0800

    Handle changing to next scene

--- Assets/Scripts/TransitionController.cs
+++ Assets/Scripts/TransitionController.cs
@@ -8,16 +8,33 @@ public class TransitionController : MonoBehaviour {
 	void Start () {
 		animateCanvas = GameObject.Find ("AnimateCanvas");
 		ct = animateCanvas.GetComponent<CompositeText> ();
+		ct.color = Color.white;
 		Invoke ("animateString", 1.0f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (Input.GetButtonDown ("Fire1")) {
+			if (ct.isCompleted == false) {
+				print ("Fire");
+				animateString ();
+			} else {
+				// TBD:
+				print("Change to next scene");
+				StartGame();
+
+			}
+		}
 	}
 
 	void animateString() {
-		ct.color = Color.white;
+		
 		ct.DOText ("你好, 我是月薪87, 安安安安安安安安");
+	}
+
+	public void StartGame()
+	{
+		UnityEngine.SceneManagement.SceneManager.LoadScene("PreviewScene-chap2", UnityEngine.SceneManagement.LoadSceneMode.Single);
+
 	}
 }
