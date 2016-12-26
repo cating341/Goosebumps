95a398318bde9f91a2f586691158addf0fcb8e25
hellosirandy <sac840711@gmail.com>
2016-12-26 17:14:17 +0800

    fixxing chap2 king monster

---
+++ Assets/AI-Scripts/Chap2KingProperties.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Chap2KingProperties : MonoBehaviour {
+
+	private GameObject player;
+	private BasicProperties basicProperties;
+	// Use this for initialization
+	void Start () {
+		player = GameObject.Find ("Player");
+		basicProperties = GetComponent<BasicProperties> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+//		print ("player: " + player.transform.position);
+//		print ("monster: " + transform.position);
+		Invoke ("teleport", 2.0f);
+	}
+
+	private void teleport(){
+		transform.position = new Vector3 (transform.position.x, player.transform.position.y, transform.position.z);
+		Invoke ("teleport", 2.0f);
+	}
+}
