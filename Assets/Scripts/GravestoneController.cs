ef231993e6d7a03fd8ea940865f8111d629e5273
cating341 <gatto0616@gmail.com>
2016-12-27 22:05:37 +0800

    add gravestone animation & prefab

---
+++ Assets/Scripts/GravestoneController.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GravestoneController : MonoBehaviour {
+
+    public bool shine;
+    private GameObject steppedMonster;
+    Animator anim;
+    bool monsterTrigger;
+   
+
+    void Start () {
+        shine = false;
+        monsterTrigger = false;
+        anim = gameObject.GetComponentInChildren<Animator>();
+    }
+
+    void Update () {
+	}
+
+    void OnTriggerEnter(Collider other)
+    { 
+        if (other.gameObject.tag == "Monster" && shine)
+        {
+            monsterTrigger = true;
+            steppedMonster = other.gameObject;
+            steppedMonster.GetComponent<BasicProperties>().NewDisability("faint", true);
+            steppedMonster.GetComponentInChildren<Animator>().SetBool("faint", true);
+
+            Debug.Log("Trigger shine gravestone event");
+        }
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player" && Input.GetKey("z"))
+        {
+            shine = true;
+            anim.SetBool("shine", true);
+            Invoke("WakeUp", 3f);
+        }
+    }
+
+    private void WakeUp()
+    {
+        shine = false;
+        anim.SetBool("shine", false);
+        if (monsterTrigger)
+        {
+            monsterTrigger = false;
+            steppedMonster.GetComponent<BasicProperties>().NewDisability("faint", false);
+            steppedMonster.GetComponentInChildren<Animator>().SetBool("faint", false);
+        }
+    }
+
+}
