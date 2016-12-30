d887d43e996b8167b134edcaba5b2a757725d9fb
hellosirandy <sac840711@gmail.com>
2016-12-30 15:41:31 +0800

    finish soldier monster 2

--- Assets/AI-Scripts/SoldierProperties.cs
+++ Assets/AI-Scripts/SoldierProperties.cs
@@ -6,34 +6,44 @@ public class SoldierProperties : MonoBehaviour {
 	private NavMeshAgent agent;
 	private GameObject player;
 	private BasicProperties basicProperties;
-	private float sceneWide = 10.8f;
+	public float leftBound;
+	public float rightBound;
+	private float heading;
 	// Use this for initialization
 	void Start () {
 		player = GameObject.Find ("Player");
 		agent = GetComponent<NavMeshAgent> ();
 		basicProperties = GetComponent<BasicProperties> ();
+		heading = rightBound;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		print (transform.position.x);
 		basicProperties.NavigateMonster (GetTargetPosition());
 		GameObject attraction = basicProperties.GetAttractionPeek ();
 		Physics.IgnoreCollision (GetComponent<Collider> (), player.GetComponent<Collider> ()
-			, basicProperties.CheckDisability() || (attraction && attraction.GetComponent<AIInformation> ().floor == GetComponent<AIInformation> ().floor));
+			, basicProperties.CheckDisability() || (attraction && attraction.name != "Player" && attraction.GetComponent<AIInformation> ().floor == GetComponent<AIInformation> ().floor));
 	}
 
 	private Vector3 GetTargetPosition() {
 		GameObject attraction = basicProperties.GetAttractionPeek ();
-		if (attraction && attraction.GetComponent<AIInformation> ().floor == GetComponent<AIInformation> ().floor) {
+		if (attraction && attraction.name != "Player" && attraction.GetComponent<AIInformation> ().floor == GetComponent<AIInformation> ().floor) {
 			return attraction.transform.position;
 		}
-		if (Vector3.Distance (new Vector3 (sceneWide, transform.position.y, transform.position.z), transform.position) < 0.3) {
-			sceneWide = sceneWide * -1;
+		if (Vector3.Distance (new Vector3 (heading, transform.position.y, transform.position.z), transform.position) < 0.3) {
+			if (heading == leftBound)
+				heading = rightBound;
+			else
+				heading = leftBound;
 		}
-		return new Vector3(sceneWide, transform.position.y, transform.position.z);
+		return new Vector3(heading, transform.position.y, transform.position.z);
 	}
 
-	public void TurnWay() {
-		sceneWide = sceneWide * -1;
+	public void TurnWay(){
+		if (heading == leftBound)
+			heading = rightBound;
+		else 
+			heading = leftBound;
 	}
 }
