47bbb6cf369bbe23495b3a3ee5bc14ae62766e06
Blurry_fish <acht7111020@gmail.com>
2016-12-30 17:05:24 +0800

    fix potion bug and add to hungry girl

---
+++ Assets/TestMovement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestMovement : MonoBehaviour {
+
+	Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float x = Input.GetAxis ("Horizontal");
+		gameObject.transform.position += new Vector3 (x/100, 0, 0);
+		animator.SetFloat ( "speed",x);
+	}
+}
