86d6bf874a1e356e548a218f2a8b8963d783cb07
hellosirandy <sac840711@gmail.com>
2017-01-07 16:35:18 +0800

    monster appear 30s

---
+++ Assets/AI-Scripts/Basic.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Basic : MonoBehaviour {
+
+
+	private Dictionary<string, bool> disabledList;
+	private Queue<GameObject> attractions;
+
+	// Use this for initialization
+	void Start () {
+		disabledList = new Dictionary<string, bool> ();
+		attractions = new Queue<GameObject> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
