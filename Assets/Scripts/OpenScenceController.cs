33a75d38ae2b569c80831b4e3be55cf7fa02a513
Shawn <san.an.lee@gmail.com>
2017-01-10 23:17:08 +0800

    Add start leaderboard

--- Assets/Scripts/OpenScenceController.cs
+++ Assets/Scripts/OpenScenceController.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class OpenScenceController : MonoBehaviour {
 
@@ -7,17 +8,23 @@ public class OpenScenceController : MonoBehaviour {
 	public GameObject StartScene;
 	public GameObject RankCanvas;
 	public GameObject[] gameview;
+	public Text Score;
+	public Text Rank;
+	public Text Name;
+
+	LeaderBoardController leaderboardController;
 
 	int maxSceneCount = 2;
 	int currentSelect = 0;
 
-	int rankingChap = 1;
+	int rankingChap = 0;
 	int rankingDifficulty = 0;
 	// Use this for initialization
 	void Start () {
 		SelectScene.SetActive (false);
 		RankCanvas.SetActive (false);
 		maxSceneCount = gameview.Length - 1;
+		leaderboardController = GetComponent<LeaderBoardController> ();
 	}
 	
 	// Update is called once per frame
@@ -59,6 +66,10 @@ public class OpenScenceController : MonoBehaviour {
 	public void PopUpRankingCanvas(){
 		RankCanvas.SetActive (true);
 		StartScene.SetActive (false);
+		StartCoroutine (leaderboardController.GetLeaderBoard (rankingChap + 1, rankingDifficulty, (lb) => {
+			TextClean();
+			SetLeaderBoard(lb);
+		}));
 	} 
     public void BackToStartScene()
     {
@@ -75,9 +86,33 @@ public class OpenScenceController : MonoBehaviour {
 
 	public void SetRankingChap(int index){
 		rankingChap = index;
+		StartCoroutine (leaderboardController.GetLeaderBoard (rankingChap + 1, rankingDifficulty, (lb) => {
+			TextClean();
+			SetLeaderBoard(lb);
+		}));
 	}
 
 	public void SetRankingDifficulty(int index){
 		rankingDifficulty = index;
+		StartCoroutine (leaderboardController.GetLeaderBoard (rankingChap + 1, rankingDifficulty, (lb) => {
+			TextClean();
+			SetLeaderBoard(lb);
+		}));
+	}
+
+	private void SetLeaderBoard(LeaderBoard lb) {
+		for (int i = 0; i < lb.leaderboard.Count; i++) {
+			playerInfo playerInfo = lb.leaderboard [i];
+			Rank.text += (i + 1).ToString () + "\n"; 
+			Name.text += playerInfo.name + "\n";
+			Score.text += playerInfo.score.ToString () + "\n";
+			//			Debug.Log (playerInfo.name + ": " + playerInfo.score);
+		}
+	}
+
+	private void TextClean() {
+		Rank.text = "";
+		Name.text = "";
+		Score.text = "";
 	}
 }
