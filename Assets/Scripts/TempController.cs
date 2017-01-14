1405b714b79e2c34c9b87d2cc116086525240c62
cating341 <gatto0616@gmail.com>
2017-01-15 01:29:07 +0800

    magic power animation

--- Assets/Scripts/TempController.cs
+++ Assets/Scripts/TempController.cs
@@ -7,6 +7,7 @@ public class TempController : MonoBehaviour {
     public RectTransform tempBar;
     public Image tempBackground;
     public Sprite[] tempBackgroundSprite;
+    public GameObject hot, cold;
 
     public float maxTemp = 88.0f;
     public float speed = 2;
@@ -17,14 +18,23 @@ public class TempController : MonoBehaviour {
     private float currentTemp ;
     private int currentState; // 3:hot 2:normal 1:cold
 
-	// Use this for initialization
-	void Start () {
+    AudioSource se;
+
+    // Use this for initialization
+    void Start () {
         currentState = 2;
         tempBackground.sprite = tempBackgroundSprite[1];
         middleTemp = (maxTemp + minTemp) / 2;
         currentTemp = middleTemp;
         tempBar.sizeDelta = new Vector2(currentTemp, tempBar.sizeDelta.y);
-	}
+        hot = GameObject.FindWithTag("hot");
+        cold = GameObject.FindWithTag("cold");
+
+        hot.GetComponent<ParticleSystem>().Stop();
+        cold.GetComponent<ParticleSystem>().Stop();
+
+        se = GetComponent<AudioSource>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -46,15 +56,30 @@ public class TempController : MonoBehaviour {
         
 	}
 
+    void StopRing()
+    {
+        hot.GetComponent<ParticleSystem>().Stop();
+        cold.GetComponent<ParticleSystem>().Stop();
+        se.Stop();
+    }
+
     public void IncreaseTemp() {
         currentState = (currentState == 3) ? 3 : currentState + 1;
         tempBackground.sprite = tempBackgroundSprite[currentState-1];
+        StopRing();
+        hot.GetComponent<ParticleSystem>().Play();
+        se.Play();
+        Invoke("StopRing", 1.5f);
     }
 
     public void DecreaseTemp()
     {
         currentState = (currentState == 1) ? 1 : currentState - 1;
         tempBackground.sprite = tempBackgroundSprite[currentState - 1];
+        StopRing();
+        cold.GetComponent<ParticleSystem>().Play();
+        se.Play();
+        Invoke("StopRing", 1.5f);
     }
      
     public float GetTemp()
