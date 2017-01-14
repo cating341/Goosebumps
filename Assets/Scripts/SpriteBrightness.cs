f586b83e108f54ae63f8858b82c87671d9d4bd71
blurry fish <acht7111020@gmail.com>
2017-01-15 00:36:45 +0800

    modify game scene 2 lighting

--- Assets/Scripts/SpriteBrightness.cs
+++ Assets/Scripts/SpriteBrightness.cs
@@ -3,28 +3,35 @@ using System.Collections;
 public class SpriteBrightness : MonoBehaviour {
 
     GameObject player;
+    MySceneManager sceneManager;
 
 	// Use this for initialization
 	void Start () {
         player = GameObject.Find("Player");
+        sceneManager = GameObject.Find("SceneManager").GetComponent<MySceneManager>();
 	}
 	
 	// Update is called once per frame
     void Update()
     {
-        if (GameObject.Find("Player"))
+        if (gameObject && player)
         {
             float y = player.transform.position.y - gameObject.transform.position.y;
-            if (Vector3.Distance(player.transform.position, gameObject.transform.position) < 4.0f && Mathf.Abs(y) < 3.5f)
+            if (sceneManager.GetLevel() == 1)
             {
-                gameObject.GetComponent<SpriteRenderer>().color = Color.white;
-                Debug.Log("white");
+                if (Vector3.Distance(player.transform.position, gameObject.transform.position) < 4.0f && Mathf.Abs(y) < 3.5f)
+                    gameObject.GetComponent<SpriteRenderer>().color = Color.white;
+                else
+                    gameObject.GetComponent<SpriteRenderer>().color = new Color(0.3f, 0.3f, 0.3f, 1);
             }
             else
             {
-                gameObject.GetComponent<SpriteRenderer>().color = new Color(0.3f, 0.3f, 0.3f, 1);
-                Debug.Log("black");
+                if (Vector3.Distance(player.transform.position, gameObject.transform.position) < 3.7f && Mathf.Abs(y) < 2.5f)
+                    gameObject.GetComponent<SpriteRenderer>().color = Color.white;
+                else
+                    gameObject.GetComponent<SpriteRenderer>().color = new Color(0.3f, 0.3f, 0.3f, 1);
             }
+
         }
     }
 }
